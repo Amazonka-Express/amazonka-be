b00080d681171f3c21ee504919ab5d4f9da41873
Jakub Kowieski <kubakowieski5151@gmail.com>
2025-06-03 01:29:01 +0200

    jk: minor changes v2

---
+++ genPbs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# must be run from the root of the repo,
+# to use install protoc: https://protobuf.dev/installation/
+
+protoc --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts --ts_out src/lib/server/pb --proto_path src/lib/server/protos src/lib/server/protos/*.proto
